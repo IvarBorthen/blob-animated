5dd70abe05826547d5a1b61ca0fe8874833b707c
Ivar Borthen <ivarborthen@Ivars-MBP.lan>
2019-09-29 22:45:55 +0200

    tsc

---
+++ lib/index.d.ts
@@ -0,0 +1,94 @@
+export declare type VectorType = {
+    x: number;
+    y: number;
+};
+export declare type BlobType = {
+    speed: number;
+    debug: boolean;
+    scramble: number;
+    size: number;
+    vectors?: VectorType[];
+    play: boolean;
+};
+declare type BlobParamTypes = {
+    canvas: HTMLCanvasElement;
+    vectors?: VectorType[];
+    speed?: number;
+    scramble?: number;
+    color?: string;
+    autoPlay?: boolean;
+    size?: number;
+    debug?: boolean;
+    cover?: boolean;
+    changedVectorsCallback?: (newVectors: VectorType[]) => void;
+    maskedElement?: HTMLImageElement | HTMLVideoElement;
+};
+declare type PointsType = {
+    initialX: number;
+    initialY: number;
+    x: number;
+    y: number;
+    xFrom: number;
+    yFrom: number;
+    xTarget: number;
+    yTarget: number;
+    bornX: number;
+    willDieX: number;
+    bornY: number;
+    willDieY: number;
+};
+declare type GeneratePointsType = {
+    sides: number;
+    scramblePercentage?: number;
+    ratio?: number;
+};
+export declare const generatePoints: ({ sides, ratio, scramblePercentage }: GeneratePointsType) => VectorType[];
+declare class Blob {
+    _canvas: HTMLCanvasElement;
+    _ctx: CanvasRenderingContext2D | null;
+    _speed: number;
+    _size: number;
+    _scramble: number;
+    _color?: string;
+    _numberOfPoints: number;
+    _isPlaying: boolean;
+    _frame: number;
+    _points: PointsType[];
+    _cover: boolean;
+    _debug?: boolean;
+    _isDragging: boolean;
+    _changedVectorsCallback?: (newVectors: VectorType[]) => void;
+    _dragIndex: number;
+    _maskedElement?: HTMLImageElement | HTMLVideoElement;
+    _mousePositions?: VectorType;
+    _createPoints: (points: VectorType[], scramble: number, size: number, speed: number) => PointsType[];
+    _easeInOutQuad: (n: number) => number;
+    _getCurvedPathPoints: (obj: {
+        x: number;
+        y: number;
+        i: number;
+        invert: -1 | 1;
+    }) => {
+        x: number;
+        y: number;
+    };
+    _updatePositions: (point: PointsType) => void;
+    _getAngleRadians: (obj: {
+        x1: number;
+        y1: number;
+        x2: number;
+        y2: number;
+    }) => number;
+    _getDistance: (vector1: VectorType, vector2: VectorType) => number;
+    _draw: () => void;
+    _debugModeChanged: (debugMode: boolean) => void;
+    constructor({ canvas, color, vectors, speed, scramble, autoPlay, cover, size, maskedElement, debug, changedVectorsCallback, }: BlobParamTypes);
+    play: boolean;
+    speed: number;
+    debug: boolean;
+    vectors: VectorType[];
+    scramble: number;
+    size: number;
+    maskedElement: HTMLImageElement | HTMLVideoElement | undefined;
+}
+export default Blob;
